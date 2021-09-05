5605aa4d9681b753de09136bd63c3443e72af813
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-09-06 02:06:58 +0900

    Fix : :fire: 소환사명 검색시 에러 처리
    소환사명 검색시 소환사가 없는 경우 에러처리

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -5,18 +5,26 @@ const Search = () => {
   const [searchValue, setValue] = useState<string>('');
 
   const getMatchTeamCode = async () => {
-    const { data } = await axios.get(`http://3.34.111.116:8070/v1/match/status/${searchValue}`);
-    const { matchTeamCode } = data.data;
-    return matchTeamCode;
+    try {
+      const { data } = await axios.get(`http://3.34.111.116:8070/v1/match/status/${searchValue}`);
+      const { matchTeamCode } = data.data;
+      return matchTeamCode;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const onClickSearchBtn = async () => {
-    const { data } = await axios.get(`http://3.34.111.116:8070/v1/match/${searchValue}`);
-    if (data.data.matchStatus === false) {
-      alert('게임 시작 안함');
-    } else {
-      const matchTeamCode = await getMatchTeamCode();
-      window.location.href = `/game/${matchTeamCode}`;
+    try {
+      const { data } = await axios.get(`http://3.34.111.116:8070/v1/match/${searchValue}`);
+      if (data.data.matchStatus === false) {
+        alert('게임 시작 안함');
+      } else {
+        const matchTeamCode = await getMatchTeamCode();
+        window.location.href = `/game/${matchTeamCode}`;
+      }
+    } catch (err) {
+      alert('없는 소환사');
     }
   };
 

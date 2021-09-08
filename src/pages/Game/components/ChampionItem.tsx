193e30cce984cf1d7e8201f6430d1819d3aedc6b
Seunghyo <seunghyoku@gmail.com>
2021-09-09 05:33:09 +0900

    Style: :high_heel: 잠금 화면 변경 - 필요 없는 컴포넌트 삭제 (기존의 시간 변경 버튼) - 필요한 타입 추가 - 함수 위치 변경 (하위 컴포넌트에서 상위인 Item으로 변경)

--- src/pages/Game/components/ChampionItem.tsx
+++ src/pages/Game/components/ChampionItem.tsx
@@ -3,18 +3,21 @@ import React, {
 } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import TimeButton from '@/common/components/TimeButton';
 import RefreshIcon from '@/common/components/RefreshIcon';
+import LockerIcon from '@/common/components/LockerIcon';
 import SpellIconInItem from './SpellIconInItem';
 
 import GameContext from '../models/context/GameContext';
+import { ChampData } from '../models/type';
 
 import './ChampionItem.scss';
 
-const ChampionItem = ({
-  champData, spellType,
-}) => {
-  const [counter, setCounter] = useState(0);
+interface ChampionItemProps {
+  champData: ChampData;
+  spellType: string;
+}
+
+const ChampionItem = ({ champData, spellType }: ChampionItemProps) => {
   const [status, setStatus] = useState('default');
   const curtainTimer = useRef(null);
   const { t } = useTranslation();
@@ -24,8 +27,13 @@ const ChampionItem = ({
   } = useContext(
     GameContext,
   );
+
   const { src, time, level } = champData.spells[spellType];
   const { summonerName } = champData;
+
+  useEffect(() => {
+    if (!time || time < 0) setStatus('default');
+  }, [status]);
 
   const handleClickIcon = () => {
     // updateUltLevel();
@@ -47,19 +55,23 @@ const ChampionItem = ({
     resetSpell(summonerName, spellType);
   };
 
-  const handleClickTime = (beforeSec) => () => {
+  const handleClickTime = (beforeSec: number) => () => {
     createCurtain();
     setStatus('modify');
     onUseSpell(summonerName, spellType, beforeSec);
   };
 
-  const changeToModityMode = (e) => {
+  const changeToModifyMode = (e) => {
     e.stopPropagation();
     createCurtain();
     setStatus('modify');
   };
 
-  if (status === 'default' || !time) {
+  const handleClickTimeButton = (t: number) => () => {
+    updateTimeUsed(summonerName, spellType, time + t >= 0 ? time + t : 0);
+  };
+
+  if (status === 'default') {
     if (spellType === 'R') {
       return (
         <div className="ChampionItem">
@@ -125,7 +137,7 @@ const ChampionItem = ({
   }
   if (status === 'modify') {
     return (
-      <div className="ChampionItem panel panel-clicked" onClick={changeToModityMode}>
+      <div className="ChampionItem" onClick={changeToModifyMode}>
         <div className="item-left">
           <SpellIconInItem
             spellType={spellType}
@@ -133,34 +145,27 @@ const ChampionItem = ({
             src={src}
             level={level}
           />
-
           <span className="leftTime">{time}s</span>
         </div>
         <div className="item-right">
-          <div className="timeButtons">
-            <TimeButton
-              time="+10"
-              leftTime={time}
-              summonerName={summonerName}
-              spellType={spellType}
-              updateTimeUsed={updateTimeUsed}
-            />
-            <TimeButton
-              time="-10"
-              leftTime={time}
-              summonerName={summonerName}
-              spellType={spellType}
-              updateTimeUsed={updateTimeUsed}
-            />
+          <div className="time-button" onClick={handleClickTimeButton(10)}>
+            +10s
+          </div>
+          <div className="line" />
+          <div className="time-button" onClick={handleClickTimeButton(-10)}>
+            -10s
+          </div>
+          <div className="line" />
+          <div className="refresh-button" onClick={onResetSpellTime}>
+            <RefreshIcon />
           </div>
         </div>
-        <RefreshIcon onResetSpellTime={onResetSpellTime} />
       </div>
     );
   }
 
   return (
-    <div className="ChampionItem panel panel-wait" onClick={changeToModityMode}>
+    <div className="ChampionItem" onClick={changeToModifyMode}>
       <div className="item-left">
         <SpellIconInItem
           spellType={spellType}
@@ -168,10 +173,12 @@ const ChampionItem = ({
           src={src}
           level={level}
         />
+        {/* TODO: 기본 초기 시간으로 리셋 */}
         <span className="leftTime">{time || 300}s</span>
       </div>
-      <div className="item-right">
-        <span className="modify-button">눌러서 수정하기</span>
+      <div className="item-right-wait">
+        <LockerIcon />
+        <span>잠금해제</span>
       </div>
     </div>
   );
--- src/pages/Game/components/SpellIconInItem.tsx
+++ src/pages/Game/components/SpellIconInItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface SpellIconInItemProps {
   spellType: string;
-  handleClick: (event: React.MouseEvent<HTMLDivElement>)=> void;
+  handleClick: (e: React.MouseEvent)=> void;
   src: string;
   level?: number;
 }

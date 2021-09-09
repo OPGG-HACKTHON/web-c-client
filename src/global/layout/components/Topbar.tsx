5626f3641df7ee2b08352a66ce4b4d731ba445ac
DESKTOP-22I3PMO\moonj <moonjoungyoung@gmail.com>
2021-09-10 04:19:04 +0900

    Feat : 로고 조정 및 클릭 시 홈으로 이동
    (cherry picked from commit fd3189e2e306f5ddbd89ec64215d4292b83dc9e6)

--- src/global/layout/components/Topbar.tsx
+++ src/global/layout/components/Topbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './Topbar.scss';
 
@@ -12,12 +13,14 @@ interface TopbarComponentProps {
 }
 
 const Topbar = ({ onClickInfoBtn, onClickShareBtn }: TopbarComponentProps) => {
+  const history = useHistory();
+
   return (
     <div
       id="Topbar"
       className="Topbar"
     >
-      <img src={logo} alt="스우미 로고" className="logo-imgage" />
+      <img src={logo} alt="스우미 로고" className="logo-image" onClick={() => history.push('/')} />
       <div className="Topbar-PrivateLayout">
         <img src={info} alt="스우미 정보 버튼" className="info-image" onClick={() => onClickInfoBtn()} />
         <img src={share} alt="링크 공유 버튼" className="share-image" onClick={() => onClickShareBtn()} />

20f6215fafa3f99ae6582cf8e677e156b375276e
DESKTOP-22I3PMO\moonj <moonjoungyoung@gmail.com>
2021-09-10 05:30:34 +0900

    Feat : 배포 sourceMap 변경으로 용량 조정

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = async (outSideEnv = {}) => {
       env.PUBLIC_PATH = `${env.CLIENT_BASE_URL}/`;
       env.SERVICE_MODE = 'prod';
       localOptions = {
-        devtool: 'eval-source-map',
+        devtool: 'source-map',
       };
       break;
 

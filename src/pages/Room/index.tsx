14cb670036a06eea50b4680e4b974ba76f88d58b
Seunghyo <seunghyoku@gmail.com>
2021-09-11 22:52:07 +0900

    Chore: :package: 필요 없는 commit 삭제

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useRef } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import axios from '@/common/helper/axios';
-import useInterval from '@/common/hooks/useInterval';
-import RoomHeader from './components/RoomHeader';
-import ShareButton from './components/ShareButton';
-import SummonerContainer from './components/SummonerContainer';
+import axios from "@/common/helper/axios";
+import useInterval from "@/common/hooks/useInterval";
+import RoomHeader from "./components/RoomHeader";
+import ShareButton from "./components/ShareButton";
+import SummonerContainer from "./components/SummonerContainer";
 
-import './index.scss';
+import "./index.scss";
 
-import MainImg from '@/common/images/MainImg.png';
+import MainImg from "@/common/images/MainImg.png";
 
 const Room = () => {
   const [showShare, setShowShare] = useState(false);
@@ -20,12 +20,11 @@ const Room = () => {
   const history = useHistory();
 
   const nameIndex = 1;
-  const summonerName = location.pathname.split('/room/')[nameIndex];
+  const summonerName = location.pathname.split("/room/")[nameIndex];
 
   const getMatchTeamCode = async () => {
     try {
       const { data } = await axios.get(`/v1/match/status/${summonerName}`);
-      console.log(data);
       const { matchTeamCode } = data.data;
       return matchTeamCode;
     } catch (err) {
@@ -37,7 +36,6 @@ const Room = () => {
   const isMatchStarted = async () => {
     try {
       const { data } = await axios.get(`/v1/match/${summonerName}`);
-      console.log(summonerName, data.data.matchStatus);
       if (data.data.matchStatus) {
         const matchTeamCode = await getMatchTeamCode();
         history.push(`/game/${matchTeamCode}`);
@@ -54,7 +52,7 @@ const Room = () => {
     setShowShare(true);
     const urlElem = urlRef.current as HTMLInputElement;
     urlElem.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     if (messageTimer.current) {
       clearTimeout(messageTimer.current);
     }
@@ -64,22 +62,22 @@ const Room = () => {
   };
 
   return (
-    <div className="Room">
+    <div className='Room'>
       <RoomHeader />
-      <div className="room-container">
+      <div className='room-container'>
         <SummonerContainer
-          src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon4904.jpg?image=q_auto:best&v=1518361200"
+          src='https://opgg-static.akamaized.net/images/profile_icons/profileIcon4904.jpg?image=q_auto:best&v=1518361200'
           summonerName={summonerName}
         />
-        <div className="room-middle">
-          <img src={MainImg} alt="메인 이미지" className="main-image" />
+        <div className='room-middle'>
+          <img src={MainImg} alt='메인 이미지' className='main-image' />
           <span>아직 게임이 시작되지 않았어요.</span>
           <span>링크 공유를 통해 팀원들을 초대해보세요!</span>
         </div>
-        <div className="room-bottom">
-          <input ref={urlRef} value="https://swoomi.me/" readOnly />
+        <div className='room-bottom'>
+          <input ref={urlRef} value='https://swoomi.me/' readOnly />
           {showShare && (
-            <div className="share-message">링크가 복사되었습니다.</div>
+            <div className='share-message'>링크가 복사되었습니다.</div>
           )}
           <ShareButton handleClickShare={handleClickShare} />
         </div>

b2aec9003144c752e1ad3b306061c57189491b18
Seunghyo <seunghyoku@gmail.com>
2021-09-12 19:12:55 +0900

    Feat: :zap: 메세지 팝업 컴포넌트로 뺀 다음에 없는 소환사일 경우 적용

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import axios from '@/common/helper/axios';
+import ToastMessage from '@/common/components/ToastMessage';
 import MainIcon from './components/MainIcon';
 import MiddelBox from './components/MiddleBox';
 import SearchBar from './components/SearchBar';
@@ -13,7 +14,9 @@ const Search = () => {
   const history = useHistory();
   const [isFocusInput, setIsFocusInput] = useState(false);
   const [searchValue, setValue] = useState<string>('');
+  const [showShare, setShowShare] = useState<boolean>(false);
 
+  const messageTimer = useRef(null);
   const isPossibleSearch = searchValue !== '';
 
   const getMatchTeamCode = async (summonerName: string) => {
@@ -41,9 +44,22 @@ const Search = () => {
         history.push(`/game/${matchTeamCode}`);
       }
     } catch (err) {
-      console.log(err);
-      alert('없는 소환사');
+      console.log('되니');
+      showMessage();
     }
+  };
+
+  const showMessage = () => {
+    console.log('안된다');
+    setShowShare(true);
+    if (messageTimer.current) {
+      clearTimeout(messageTimer.current);
+    }
+
+    messageTimer.current = setTimeout(() => {
+      setShowShare(false);
+      messageTimer.current = null;
+    }, 1500);
   };
 
   React.useEffect(() => {
@@ -58,7 +74,14 @@ const Search = () => {
     <div className="Search-Page-Wrapper">
       <MainIcon />
       <MiddelBox />
-      <SearchBar value={searchValue} setValue={setValue} setIsFocusInput={setIsFocusInput} />
+      <SearchBar
+        value={searchValue}
+        setValue={setValue}
+        setIsFocusInput={setIsFocusInput}
+      />
+      {showShare && (
+        <ToastMessage content="닉네임을 확인해주세요." time={1500} />
+      )}
       {isFocusInput || isPossibleSearch ? (
         <SearchButton
           onClick={onClickSearchBtn}

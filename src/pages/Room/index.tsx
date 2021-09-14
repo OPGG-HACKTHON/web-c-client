4bbc3c1373dc0d875a95f28043a23b4c352006bb
Seunghyo <seunghyoku@gmail.com>
2021-09-14 23:47:46 +0900

    Feat: 소환사 아이콘 가져오기 - 소환사 아이콘을 불러와 적용하기

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import MainImg from '@/common/images/MainImg.png';
@@ -14,6 +14,7 @@ import './index.scss';
 
 const Room = () => {
   const [showShare, setShowShare] = useState<boolean>(false);
+  const [profile, setProfile] = useState<string>('');
   const messageTimer = useRef(null);
   const urlRef = useRef();
 
@@ -22,7 +23,6 @@ const Room = () => {
 
   const nameIndex = 1;
   const summonerName = location.pathname.split('/room/')[nameIndex];
-
   const getMatchTeamCode = async () => {
     try {
       const { data } = await axios.get(`/v1/match/status/${summonerName}`);
@@ -34,6 +34,24 @@ const Room = () => {
     }
   };
 
+  const isValidUser = async () => {
+    try {
+      const { data } = await axios.get(`/v1/summoner/${summonerName}`);
+      if (data) {
+        const { data: img } = await axios.get(
+          `v1/profileImgURL?summonerName=${summonerName}`,
+        );
+        setProfile(img.data);
+      }
+    } catch (err) {
+      history.push('/');
+    }
+  };
+
+  useEffect(() => {
+    isValidUser();
+  }, []);
+
   const isMatchStarted = async () => {
     try {
       const { data } = await axios.get(`/v1/match/${summonerName}`);
@@ -42,7 +60,8 @@ const Room = () => {
         history.push(`/game/${matchTeamCode}`);
       }
     } catch (err) {
-      console.error(err);
+      console.log('Match Not Started');
+      // console.error(err);
     }
   };
 
@@ -69,17 +88,18 @@ const Room = () => {
     <div className="Room">
       <RoomHeader />
       <div className="room-container">
-        <SummonerContainer
-          src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon4904.jpg?image=q_auto:best&v=1518361200"
-          summonerName={summonerName}
-        />
+        <SummonerContainer src={profile} summonerName={summonerName} />
         <div className="room-middle">
           <img src={MainImg} alt="메인 이미지" className="main-image" />
           <span>아직 게임이 시작되지 않았어요.</span>
           <span>링크 공유를 통해 팀원들을 초대해보세요!</span>
         </div>
         <div className="room-bottom">
-          <input ref={urlRef} value="https://swoomi.me/" readOnly />
+          <input
+            ref={urlRef}
+            value={`https://swoomi.me${location.pathname}`}
+            readOnly
+          />
           {showShare && (
             <ToastMessage content="링크가 복사되었습니다." time={1500} />
           )}

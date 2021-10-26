fd2ea56408b4a86625e85a10a1edfd14e1e70138
Seunghyo <seunghyoku@gmail.com>
2021-10-26 23:52:32 +0900

    Style: :high_heel: 대기방 스켈레톤 UI 적용 - 소환사 아이콘 가져오기 전에 alt 메세지 그대로 출력되는 것 막기 위해 - 스켈레톤 UI 적용

--- src/pages/Room/components/SummonerContainer.tsx
+++ src/pages/Room/components/SummonerContainer.tsx
@@ -9,7 +9,11 @@ interface SummonerContainerProps {
 const SummonerContainer = ({ src, summonerName }: SummonerContainerProps) => {
   return (
     <div className="SummonerContainer">
-      <img className="summoner-icon" src={src} alt="소환사아이콘" />
+      {src ? (
+        <img className="summoner-icon" src={src} alt="소환사아이콘" />
+      ) : (
+        <div className="summoner-icon" />
+      )}
       <span className="summoner-name">{summonerName}</span>
     </div>
   );
